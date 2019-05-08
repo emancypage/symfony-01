428d7090dbcb8c327bdef7f7ec5d21124e8488b9
emancypage <szymkowiak.szymon@gmail.com>
2019-05-08 21:22:17 +0200

    Initial files

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController
+{
+	/**
+	 * @var \Twig\Environment
+	 */
+	private $twig;
+	/**
+	 * @var SessionInterface
+	 */
+	private $session;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(
+	    Environment $twig,
+        SessionInterface $session,
+        RouterInterface $router
+    )
+	{
+		$this->twig = $twig;
+		$this->session = $session;
+        $this->router = $router;
+    }
+
+	/**
+	 * @Route("/", name="blog_index")
+	 */
+	public function index()
+	{
+		$html = $this->twig->render(
+		    'blog/index.html.twig',
+			[
+				'posts' => $this->session->get('posts')
+			]
+        );
+
+		return new \Symfony\Component\HttpFoundation\Response($html);
+	}
+
+	/**
+	 * @Route("/add", name="blog_add")
+	 */
+	public function add()
+	{
+		$posts = $this->session->get('posts');
+		$posts[uniqid()] = [
+			'title' => 'A random title '.rand(1,500),
+			'text' => 'A random text '.rand(1,500),
+		];
+		$this->session->set('posts', $posts);
+
+		return new RedirectResponse($this->router->generate('blog_index'));
+	}
+
+	/**
+	 * @Route("/show/{id}", name="blog_show")
+	 */
+	public function show($id)
+	{
+		$posts = $this->session->get('posts');
+
+		if (!$posts || !isset($posts[$id])) {
+			throw new NotFoundHttpException('Post not found');
+		}
+
+		$html = $this->twig->render(
+			'blog/post.html.twig',
+			[
+				'id' => $id,
+				'post' => $posts[$id]
+			]
+		);
+
+		return new Response($html);
+	}
+}
